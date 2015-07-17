e274f5adf35cbc49f2443698bd3cb401097684fe
Eric Finlay <enfinlay@gmail.com>
2015-07-17 06:34:17 -0700

    insertionSortJS - fixed bounds error and added comments
    Signed-off-by: Eric Finlay <enfinlay@gmail.com>

--- Ch2/InsertionSort/JS/insertionSort.js
+++ Ch2/InsertionSort/JS/insertionSort.js
@@ -5,13 +5,23 @@
 var insertionSort = function(inArr) {
 	var key;
 	var i;
+	// Starting at second value in Array so that we have room to swap
 	for (var j = 1; j<inArr.length; j++){
+		// key is the current value of the array
 		key = inArr[j];
+		// i is the place before j
 		i = j-1;
-		while(i>0 && inArr[i] > key) {
+		// start stepping backwards if the current array value is greather than key
+		while(i>=0 && inArr[i] > key) {
+			// swap values
 			inArr[i+1] = inArr[i];
+			// step back one
 			i = i-1;
 		}
+		// right before this line, if the loop was entered, we have two
+		// identical values in a row
+		// This replaces the first one with the key
+		// The +1 counteracts the -1 from line 19
 		inArr[i+1] = key;
 	}
 }
@@ -21,7 +31,12 @@ console.log(testArr1);
 insertionSort(testArr1);
 console.log(testArr1);
 
-var testArr2 = [1,3,2,4,5];
-console.log(testArr2);
-insertionSort(testArr2);
-console.log(testArr2);
+testArr1 = [1,3,2,4,5];
+console.log(testArr1);
+insertionSort(testArr1);
+console.log(testArr1);
+
+testArr1 = [5,4,3,2,1];
+console.log(testArr1);
+insertionSort(testArr1);
+console.log(testArr1);
